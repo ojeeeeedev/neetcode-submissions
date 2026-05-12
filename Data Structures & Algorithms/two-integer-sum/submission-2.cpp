37ff3548f52f0e988b30869b7e3373c0355fb0c3
Antonius Andar P. <andar.partogi@gmail.com>
2026-05-12 16:25:03 +0700

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        //sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < nums.size();i++) {
+            for (int j = 1; j < nums.size();j++){
+                if (nums[i] + nums[j] == target && i != j) {
+                return {i, j};
+                }
+            }
+        }
+
+        return {};
+    }
+};
